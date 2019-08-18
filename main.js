ee612393a77e855fb9b84da0dfb02f3577c1bdc0
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2019-08-18 15:56:22 +0700

    heyhey

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const socket = io('https://hiep1810.github.io');
+const socket = io('https://demo-chat-1810.herokuapp.com/');
 
 const videoElem = document.getElementById("video");
 const videoGuest = document.getElementById("videoGuest");
